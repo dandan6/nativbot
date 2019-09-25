33d6d6abb43e5810a6d996cf89a2b3503c6dc80d
dandan6 <dandan6@users.noreply.github.com>
2019-09-25 09:22:05 +0300

    Update and rename HelloWorld.py to sample.py

---
+++ sample.py
@@ -0,0 +1,40 @@
+import logging
+
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater
+
+import bot_settings
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=bot_settings.BOT_TOKEN)
+dispatcher = updater.dispatcher
+
+
+def start(bot, update):
+    chat_id = update.message.chat_id
+    logger.info(f"> Start chat #{chat_id}")
+    bot.send_message(chat_id=chat_id, text="💣 Welcome! 💣")
+
+
+def respond(bot, update):
+    chat_id = update.message.chat_id
+    text = update.message.text
+    logger.info(f"= Got on chat #{chat_id}: {text!r}")
+    response = text.replace("7", "💣")
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, respond)
+dispatcher.add_handler(echo_handler)
+
+logger.info("Start polling")
+updater.start_polling()
